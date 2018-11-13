81766eea4c8261d262c52397075ba5c006865bed
Grace Yoo <graceyoo33@gmail.com>
2018-11-13 15:26:47 -0500

    Updates

---
+++ precache-manifest.40f33c6c3e2f64bbaa7153e36603dddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "040bb672bb11de19667a",
+    "url": "/memory-game/static/js/main.040bb672.chunk.js"
+  },
+  {
+    "revision": "ddee8be47a853cc05cec",
+    "url": "/memory-game/static/js/1.ddee8be4.chunk.js"
+  },
+  {
+    "revision": "040bb672bb11de19667a",
+    "url": "/memory-game/static/css/main.4922fedf.chunk.css"
+  },
+  {
+    "revision": "d6ad2d21083064ce7b85ae4be1fa6253",
+    "url": "/memory-game/index.html"
+  }
+];
